c26f8b699bbf809cb92212856b71db4ab46f830a
zappyfish <liamkelly2015@gmail.com>
2018-07-18 20:19:18 -0700

    frame

--- sentera.cpp
+++ sentera.cpp
@@ -1,4 +1,26 @@
-#include <stdio>
+#include "sentera.h"
+
+void sentera::sentera() {
+  sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	memset((char *)&address, 0, sizeof(address));
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = inet_addr(IP); // this is address of host which I want to send the socket
+	address.sin_port = htons(atoi(PORT));
+
+
+	printf("handle: %d\n", sock); // prints number greater than 0 so I assume handle is initialized properly
+
+	if (bind(sock, (const struct sockaddr*) &address, sizeof(struct sockaddr_in)) < 0)
+	{
+		printf("failed to bind socket (%s)\n", strerror(errno)); // Cannot assign requested address
+	}
+}
+
+void sentera::~sentera() {
+
+}
 
 // Send start capture command
 void sentera::startCapture() {
@@ -23,4 +45,8 @@ void sentera::startCaptureAndTransmit() {
     while ((url = getCaptureUrl()_ == nullptr);
     getImageAndTransmit(url);
   }
+}
+
+uint8_t* sentera::assemblePacket(uint8_t type, uint16_t length, uint8_t *payload) {
+
 }
--- sentera.h
+++ sentera.h
@@ -1,12 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 
+#define IP "192.168.143.141"
+#define PORT "8080"
+
+#define HEADER_ZERO 0x46
+#define HEADER_ONE 0x57
 
 class sentera {
 public:
+  void sentera()
+  void ~sentera()
   void startCaptureAndTransmit();
 private:
   void startCapture();
   std::string getCaptureUrl();
   void getImageAndTransmit();
 
+  uint8_t* assemblePacket(uint8_t type, uint16_t length, uint8_t *payload);
+
   transmit transmitter;
+
 }
